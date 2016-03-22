2df254f81786897fd730656e1bc9d733198a6646
Ty <tytowry@gmail.com>
2016-03-22 10:21:26 -0700

    update

--- js/bip.js
+++ js/bip.js
@@ -55,3 +55,23 @@ $( document ).ready( function() {
   });
   
 });
+
+var viz, workbook;
+
+window.onload=function() {
+    var vizDiv = document.getElementById('viz');
+    var vizURL = 'https://insightwdev.usfca.edu/#/projects/';
+    var options = {
+        width: '600px',
+        height: '540px',
+        hideToolbar: true,
+        hideTabs: true
+    }
+viz = new tableauSoftware.Viz(vizDiv,vizURL, options);
+}
+
+function initViz() {
+          var containerDiv = document.getElementById("vizContainer"),
+          url = "https://insightwdev.usfca.edu/#/projects/34/workbooks";
+          var viz = new tableau.Viz(containerDiv, url); 
+          }
